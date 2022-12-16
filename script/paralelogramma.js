29d44f5b151dba03765b254d4b2e4f88a21bafbb
zetenyb <120386675+zetenyb@users.noreply.github.com>
2022-12-16 15:20:50 +0100

    felkesz

---
+++ script/paralelogramma.js
@@ -0,0 +1,11 @@
+function paralelog()
+{
+var a,b,Para;
+a = parseInt(document.getElementById ("oldal").value);
+b = parseInt(document.getElementById ("magasság").value);
+Para = a*b; //Multiplication of two numbers
+
+//To print the result
+document.getElementById ("Para").innerHTML ="A paralelogramma területe: "+Mul;
+
+}
