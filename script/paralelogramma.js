477831e705e26c5021904872dcf156f6d5f060ba
zetenyb <120386675+zetenyb@users.noreply.github.com>
2022-12-18 15:34:39 +0100

    paralelogram kász

--- script/paralelogramma.js
+++ script/paralelogramma.js
@@ -1,11 +1,17 @@
-function paralelog()
-{
-var a,b,Para;
-a = parseInt(document.getElementById ("oldal").value);
-b = parseInt(document.getElementById ("magasság").value);
-Para = a*b; //Multiplication of two numbers
+function paralelogram(){
+    var a,b,Para;
+    a = parseInt(document.getElementById ("oldal").value);
+    b = parseInt(document.getElementById ("magassag").value);
+    Para = a*b;
 
-//To print the result
-document.getElementById ("Para").innerHTML ="A paralelogramma területe: "+Mul;
+    document.getElementById ("Para").innerHTML ="A paralelogramma területe: "+Para;}
 
-}
+function paralelogram2(){
+        var c,d,e,edeg,Para2;
+        c = parseInt(document.getElementById ("coldal").value);
+        d = parseInt(document.getElementById ("doldal").value);
+        e = parseInt(document.getElementById ("szog").value);
+        edeg = e* (Math.PI/ 180)
+        Para2 = c*d*Math.sin(edeg);
+    
+        document.getElementById ("Para2").innerHTML ="A paralelogramma területe: "+Para2;}
